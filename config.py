33c144b4b759d697428393a84f09b6b19122979b
ffffffffffff984-sys <ffffffffffff984@gmail.com>
2026-06-24 16:38:39 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# 設定
+class Config(object):
+    # デバックモード
+    DEBUG=True
+    # CSRFやセッションっで使用（イテレーション02で追加）
+    SECRET_KEY = "secret-key"
+    # 警告対策
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # DB設定
+    SQLALCHEMY_DATABASE_URI = "sqlite:///memodb.sqlite"
